5dae0097fb2984fc4ffeb2ad7b798e9abcda8c33
losbojos <losbojos@yandex.ru>
2023-02-04 16:05:32 +0700

    Fix: Удалить span и вынести кнопку из заголовка в отдельный div

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const editButton = document.querySelector(".profile__edit-button");
-const profileNameText = document.getElementById("profile__name-text_id");
+const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__about");
 
 const popupEditProfile = document.querySelector(".popup-edit-profile");
@@ -11,7 +11,7 @@ const formElement = popupEditProfile.querySelector(".form-edit-profile");
 const toggleOpenPopup = () => {
   let isOpened = popupEditProfile.classList.toggle("popup-edit-profile_opened");
   if (isOpened) {
-    inputName.value = profileNameText.textContent;
+    inputName.value = profileName.textContent;
     inputAbout.value = profileAbout.textContent;
   }
 };
@@ -19,7 +19,7 @@ const toggleOpenPopup = () => {
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.  
 
-  profileNameText.textContent = inputName.value;
+  profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
   toggleOpenPopup();
 }
